4c0af8db9bee89a1da7ff027fd4bfcb190e5811d
Alice-63 <alicetindag84@gmail.com>
2021-11-05 22:41:28 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="tag-container">
-
-
-<input 
-type="text" 
-@keydown.enter="addTag"
-@keydown.backspace="removeTag"
->
-<div class="error" v-if="error">Bu etiket dah önceden eklenmis!!</div>
-  </div>
+ 
 </template>
 <script>
 export default {
--- src/Component/Tags.vue
+++ src/Component/Tags.vue
@@ -1,5 +1,14 @@
 <template>
-    
+     <div class="tag-container">
+
+
+<input 
+type="text" 
+@keydown.enter="addTag"
+@keydown.backspace="removeTag"
+>
+<div class="error" v-if="error">Bu etiket dah önceden eklenmis!!</div>
+  </div>
 </template>
 <script>
 export default {
