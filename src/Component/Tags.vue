88f735218fbb516a006e0095397a202d0a24d2ed
Alice-63 <alicetindag84@gmail.com>
2021-11-05 22:48:59 +0100

    Update Tags.vue

--- src/Component/Tags.vue
+++ src/Component/Tags.vue
@@ -15,6 +15,49 @@ import Tag from "./Tag.vue"
 export default {
     components:{
         Tag
+    },
+    methods:{
+          addTag(event){
+      let text=event.target;
+      let matchedTag=false
+      if(text.value.length>0)
+      {
+         this.tags.forEach(tag=>{
+           if(tag.toLowerCase()===text.value.toLowerCase())
+           {
+            matchedTag=true;
+           }
+      
+         })
+
+         if(!matchedTag)
+         {
+           this.tags.push(text.value);
+           text.value=""
+         }
+         else{
+           this.error=true;
+           setTimeout(()=>{
+             this.error=false;
+           },2000)
+         }
+
+
+      }
+
+      
+     
+    },
+    removeTag(e){
+
+      if(e.target.value==0){
+        
+      this.tags.splice(this.tags.length-1,1)
+      }
+    },
+      removeOneTag(index){
+      this.tags.splice(index,1)
+    }
     }
 }
 </script>
